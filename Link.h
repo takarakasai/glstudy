f797beeb2b65403348b873fc7f48ec72f7794028
kasai <takara.kasai@gmail.com>
2016-06-18 21:25:17 +0900

    change to use offset value.

--- Link.h
+++ Link.h
@@ -28,8 +28,9 @@ private:
   Eigen::Vector3d l_tippos_;
   Eigen::Matrix3d l_tiprot_;
   Dp::Math::real     mass_;
-  Eigen::Vector3d centroid_; /* center of gravity */
-  Eigen::Matrix3d cinertia_; /* inertia tensor at centroid */
+  Eigen::Vector3d centroid_; /* center of gravity at local coordinates */
+  Eigen::Vector3d wcentroid_; /* center of gravity at world coordinates */
+  Eigen::Matrix3d cinertia_; /* inertia tensor at centroid at local coordinates */
 
   std::shared_ptr<Joint>& getJoint() {
     return joint_;
@@ -45,20 +46,34 @@ public:
     const char* name, std::shared_ptr<Joint> joint, Vector3d lpos, Dp::Math::real mass, Vector3d centroid,
     Matrix3d cinertia) :
     name_(name), joint_(joint), l_tippos_(lpos), l_tiprot_(Matrix3d::Identity()), mass_(mass), centroid_(centroid),
-    cinertia_(cinertia) {};
+    wcentroid_(Eigen::Vector3d::Zero()), cinertia_(cinertia) {};
     //cinertia_(Matrix3d::Identity()) {};
   virtual ~Link() {};
 
   void SetMass(Dp::Math::real mass) {
     mass_ = mass;
   }
+  Dp::Math::real& GetMass() {
+    return mass_;
+  }
 
   void SetCentroid(Vector3d centroid) {
     centroid_ = centroid;
   }
+  Vector3d& GetCentroid() {
+    return centroid_;
+  }
+  Vector3d& GetWCentroid() {
+    /* TODO: update at UpdateCasCoords */
+    wcentroid_ = WPos() + WRot() * centroid_;
+    return wcentroid_;
+  }
 
   void SetInertia(Matrix3d inertia) {
     cinertia_ = inertia;
+  }
+  Matrix3d& GetIntertia() {
+    return cinertia_;
   }
 
   static std::shared_ptr<Link> Create (
@@ -92,6 +107,22 @@ public:
     return FindJoint(tname);
   }
 
+  /* TODO: not Link* */
+  //std::shared_ptr<Link> FindLink(std::string& str) {
+  Link* FindLink(std::string& str) {
+    /* TODO: duplicate */
+    if (GetName() == str) {
+      return this;
+    }
+    for (auto &link : clinks_) {
+      auto lnk = link->FindLink(str);
+      if (lnk != NULL) {
+        return lnk;
+      }
+    }
+    return NULL;
+  }
+
   // TODO: --> CasCoords 
   Eigen::Vector3d& LTipPos() {
     return l_tippos_;
@@ -112,11 +143,12 @@ public:
   }
 
   errno_t ApplyLocalCoords() {
+    /* pos & lot root of this link */
     //CasCoords::LRot() = joint_->Rot() * l_tiprot_;
+    //std::cout << name_ << std::endl;
     CasCoords::LRot() = l_tiprot_ * joint_->Rot();
     CasCoords::LPos() = joint_->Pos() + l_tippos_;
     //std::cout << name_ << std::endl;
-    //std::cout << "  ROT:" << CasCoords::LRot() << std::endl;
     //std::cout << "  POS:" << l_tippos_ << std::endl;
     //std::cout << "  POS:" << CasCoords::LPos() << std::endl;
     return 0;
